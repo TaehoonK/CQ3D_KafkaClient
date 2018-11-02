9bab759fded2a5b70dc75cf47f6966c4fff5c2f9
TaehoonK <taehoon.kim@pusan.ac.kr>
2018-11-02 20:26:44 +0900

    initial commit

---
+++ kafka_client_webserver.py
@@ -0,0 +1,76 @@
+import logging, json
+import urllib
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from kafka import KafkaConsumer
+
+logging.basicConfig(
+    #level=logging.DEBUG
+    level=logging.INFO
+)
+# To consume latest messages and auto-commit offsets
+consumer = KafkaConsumer('test',
+                         bootstrap_servers=['150.82.218.237:9092'],
+                         #sasl_mechanism = 'SASL_PLAINTEXT',
+                         #sasl_plain_username = 'scdemo',
+                         #sasl_plain_password = 'demosc',
+                         auto_offset_reset='earliest',
+                         enable_auto_commit=False,
+                         api_version=(0, 10),
+                         #consumer_timeout_ms=1000,
+                         #value_deserializer=lambda m: json.loads(m.decode('utf-8'))
+                         )
+#for message in consumer:
+    # message value and key are raw bytes -- decode if necessary!
+    # e.g., for unicode: `message.value.decode('utf-8')`
+    #print (message)
+    #print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
+
+# HTTP Request handlers
+class requestHandler(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/x-www-form-urlencoded')
+        self.send_header('Access-Control-Allow-Credentials', 'true')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+
+    def do_HEAD(self):
+        self._set_headers()
+
+    def do_POST(self):
+        msg = getCurrentMessage()
+        print(msg)
+        self._set_headers()
+        self.wfile.write(msg)
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'image/png')
+        self.end_headers()
+
+        o = urllib.parse.urlparse(self.path)
+        params = urllib.parse.parse_qs(o.query)
+        print(params)
+        if(bool(params)):
+            fileID = params['id'][0]
+            fileURL = 'resource/mms/'+ fileID +'.png'
+            self.wfile.write(load_binary(fileURL))
+
+def getCurrentMessage():
+    msg = next(consumer)
+    return msg.value
+
+def load_binary(file):
+    with open(file, 'rb') as file:
+        return file.read()
+
+def run():
+    print("Starting a server")
+    server_address = ("127.0.0.1", 8080)
+    httpd = HTTPServer(server_address, requestHandler)
+
+    print("Running the server")
+    httpd.serve_forever()
+
+run()
---
+++ kafka_test.py
@@ -0,0 +1,52 @@
+import threading, logging, time
+import multiprocessing
+
+from kafka import KafkaConsumer
+
+class Consumer(multiprocessing.Process):
+   def __init__(self):
+       multiprocessing.Process.__init__(self)
+       self.stop_event = multiprocessing.Event()
+
+   def stop(self):
+       self.stop_event.set()
+
+   def run(self):
+       consumer = KafkaConsumer('processed_stream',
+                                bootstrap_servers=['150.82.218.237:9092'],
+                                auto_offset_reset='latest',
+                                api_version=(0, 10),
+                                consumer_timeout_ms=1000)
+       consumer.subscribe(['processed_stream'])
+
+       while not self.stop_event.is_set():
+           for message in consumer:
+               print(message)
+               if self.stop_event.is_set():
+                   break
+
+       consumer.close()
+
+
+def main():
+   tasks = [
+       Consumer()
+   ]
+
+   for t in tasks:
+       t.start()
+
+   time.sleep(100)
+
+   for task in tasks:
+       task.stop()
+
+   for task in tasks:
+       task.join()
+
+
+if __name__ == "__main__":
+   logging.basicConfig(
+       level=logging.INFO
+   )
+   main()
